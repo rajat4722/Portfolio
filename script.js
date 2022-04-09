74272160aa985a52fea7e5133d9fe8d89af05f20
suklarajat <33422414+rajat4722@users.noreply.github.com>
2022-04-09 15:57:08 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.onclick = function(){
+    navigation.classList.toggle('open')
+}
+
+const list = document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click',activeLink))
+
+document.getElementById('learn-more').addEventListener('click',
+function(){
+document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display = 'none';
+});
